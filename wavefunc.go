fe9e62256186cb12c8188fc266a1bae1115f1859
Adam Escobedo <acescobedo@smu.edu>
2022-04-29 13:53:15 -0500

    initialized definition of the wave function collapse package

--- wavefunc.go
+++ wavefunc.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	wfcp2 "wavefunc.go/wfcp"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	v1 := wfcp2.CellElement{
+		RowExclusions: []wfcp2.CellElementer{CellWrap{Data: -1}},
+		ColExclusions: []wfcp2.CellElementer{CellWrap{Data: -1}},
+		Data: CellWrap{
+			Data: 1,
+		},
+	}
+
+	v2 := wfcp2.CellElement{
+		RowExclusions: []wfcp2.CellElementer{CellWrap{Data: 2}},
+		ColExclusions: []wfcp2.CellElementer{CellWrap{Data: -2}},
+		Data: CellWrap{
+			Data: 2,
+		},
+	}
+
+	v3 := wfcp2.CellElement{
+		RowExclusions: []wfcp2.CellElementer{CellWrap{Data: 3}},
+		ColExclusions: []wfcp2.CellElementer{CellWrap{Data: -3}},
+		Data: CellWrap{
+			Data: 3,
+		},
+	}
+
+	v4 := wfcp2.CellElement{
+		RowExclusions: []wfcp2.CellElementer{CellWrap{Data: 1}},
+		ColExclusions: []wfcp2.CellElementer{CellWrap{Data: -1}},
+		Data: CellWrap{
+			Data: -1,
+		},
+	}
+
+	v5 := wfcp2.CellElement{
+		RowExclusions: []wfcp2.CellElementer{CellWrap{Data: 2}},
+		ColExclusions: []wfcp2.CellElementer{CellWrap{Data: -2}},
+		Data: CellWrap{
+			Data: -2,
+		},
+	}
+
+	v6 := wfcp2.CellElement{
+		RowExclusions: []wfcp2.CellElementer{CellWrap{Data: 3}},
+		ColExclusions: []wfcp2.CellElementer{CellWrap{Data: -3}},
+		Data: CellWrap{
+			Data: -3,
+		},
+	}
+
+	board := wfcp2.Board{
+		Corpus: []wfcp2.CellElement{v1, v2, v3, v4, v5, v6},
+	}
+	board.EmptyBoard(10, 10)
+	board.Data[0][1] = v1
+}
+
+type CellWrap struct {
+	Data int
+}
+
+func (i CellWrap) EqualFlags(p wfcp2.CellElementer) bool {
+	return i == p
 }
--- wfcp/wfc_operative.go
+++ wfcp/wfc_operative.go
@@ -1,1 +1,31 @@
 package wfcp
+
+type CellElementer interface {
+	EqualFlags(CellElementer) bool
+} // each type that enacts cellElement needs have a unique flag that can be compared
+
+type CellElement struct {
+	rowOptions    []CellElementer
+	colOptions    []CellElementer
+	RowExclusions []CellElementer
+	ColExclusions []CellElementer
+	Data          CellElementer
+}
+
+type Board struct {
+	Data   [][]CellElement
+	Corpus []CellElement
+}
+
+func (b *Board) EmptyBoard(maxDomain, maxRange int) *Board {
+	b.Data = make([][]CellElement, maxRange)
+	for i := 0; i < maxRange; i++ {
+		b.Data[i] = make([]CellElement, maxDomain)
+	}
+	return b
+}
+
+func (b *Board) ValidateBoard() *Board {
+
+	return b
+}
